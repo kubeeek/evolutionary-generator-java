340f0faaeaf77db47f27bec7391d1db0b46f87a3
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2022-12-27 15:37:14 +0100

    updated tests

--- src/test/java/ConfigReaderTest.java
+++ src/test/java/ConfigReaderTest.java
@@ -1,3 +1,4 @@
+import agh.ConfigReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,10 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ConfigReaderTest {
     @Test
     void testValidFilename() throws IOException {
-        var configReader = new ConfigReader("default");
+        var configReader = new ConfigReader("default.properties");
         var defaultProps = configReader.read();
 
         assertEquals(15, defaultProps.size());
+        assertEquals("50", defaultProps.getProperty("width"));
+
     }
 
     @Test()
--- src/test/java/SimulationConfigReaderIntegrationTest.java
+++ src/test/java/SimulationConfigReaderIntegrationTest.java
@@ -1,39 +1,72 @@
+import agh.ConfigReader;
+import agh.SimulationConfig;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimulationConfigReaderIntegrationTest {
     @Test
     void testValidDefaultConfig() throws IOException {
-        var configReader = new ConfigReader("default");
+        var configReader = new ConfigReader("default.properties");
         var defaultProps = configReader.read();
 
         new SimulationConfig(defaultProps);
     }
 
     @Test()
-    void testInvalidDefaultConfig() {
+    void testInvalidDefaultConfigName() {
         var configReader = new ConfigReader("invalid_default");
-        Properties defaultProps = null;
 
-        try {
-            defaultProps = configReader.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        assertThrows(NullPointerException.class, configReader::read);
+    }
 
-        Properties finalDefaultProps = defaultProps;
+    @Test()
+    void testInvalidDefaultConfig() throws IOException {
+        var configReader = new ConfigReader("invalid_default.properties");
+        Properties defaultProps = new Properties();
 
-        var exception = assertThrows(IllegalArgumentException.class, () -> new SimulationConfig(finalDefaultProps));
+        var props = configReader.read();
+
+
+        var exception = assertThrows(IllegalArgumentException.class, () -> new SimulationConfig(props));
         assertEquals(exception.getMessage(), "Default config is invalid. Missing pairs.");
     }
 
-    // TODO
-    void testValidUserConfig() {}
-    void testInvalidUserConfig() {}
+    @Test
+    void testValidUserConfig() throws URISyntaxException, IOException {
+        URL res = getClass().getClassLoader().getResource("default.properties");
+        File file = Paths.get(res.toURI()).toFile();
+
+        var defaultConfigProps = new ConfigReader("default.properties").read();
+        var userConfigProps = new ConfigReader(file).read();
+
+        assertDoesNotThrow(() -> new SimulationConfig(userConfigProps, defaultConfigProps));
+
+        SimulationConfig simulationConfig = new SimulationConfig(userConfigProps, defaultConfigProps);
+        assertEquals("50", simulationConfig.getParameter("width"));
+    }
+
+    @Test
+    @Description("If the key is missing in user's config, a default value should be returned")
+    void testInvalidUserConfig() throws URISyntaxException, IOException {
+        URL res = getClass().getClassLoader().getResource("invalid_default.properties");
+        File file = Paths.get(res.toURI()).toFile();
+
+        var defaultConfigProps = new ConfigReader("default.properties").read();
+        var userConfigProps = new ConfigReader(file).read();
+
+        assertDoesNotThrow(() -> new SimulationConfig(userConfigProps, defaultConfigProps));
+
+        SimulationConfig simulationConfig = new SimulationConfig(userConfigProps, defaultConfigProps);
+        assertEquals("5", simulationConfig.getParameter("energy_reproduce_loss"));
+    }
 
 }
