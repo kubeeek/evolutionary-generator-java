8e053f09d947889f9af1f3346acdd15e8ff13036
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2022-12-27 15:38:35 +0100

    fixed package issues

--- src/main/java/agh/SimulationConfig.java
+++ src/main/java/agh/SimulationConfig.java
@@ -1,10 +1,11 @@
+package agh;
+
 import java.util.Properties;
 
 
 public class SimulationConfig {
     private Properties defaultProp;
-    private final Properties userProp;
-    final int expectedConfigPairs = 15;
+    private Properties userProp;
 
     public SimulationConfig(Properties userProp, Properties defaultProp) {
         validateDefaultConfig(defaultProp);
@@ -17,14 +18,19 @@ public class SimulationConfig {
     }
 
     private void validateDefaultConfig(Properties defaultProp) {
-        if (defaultProp.size() < this.expectedConfigPairs) {
+        int expectedConfigPairs = 15;
+        if (defaultProp.size() < expectedConfigPairs) {
             throw new IllegalArgumentException("Default config is invalid. Missing pairs.");
         }
 
         this.defaultProp = defaultProp;
     }
 
-    String getParameter(String key) {
+    void setUserConfig(Properties userProp) {
+        this.userProp = userProp;
+    }
+
+    public String getParameter(String key) {
         return this.userProp.getProperty(key, this.defaultProp.getProperty(key));
     }
 }
